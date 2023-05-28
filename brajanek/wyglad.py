bbb351de19e553f85c7d03c2e7ef73d088773165
Iza <lilamovie@wp.pl>
2023-05-28 18:59:23 +0200

    Initial commit

---
+++ brajanek/wyglad.py
@@ -0,0 +1,31 @@
+from pygame import image
+
+bieg_A_lewanoga = image.load("assets/brajanek/bieg_A_lewanoga.png")
+bieg_A_prawanoga = image.load("assets/brajanek/bieg_A_prawanoga.png")
+bieg_D_lewanoga = image.load("assets/brajanek/bieg_D_lewanoga.png")
+bieg_D_prawanoga = image.load("assets/brajanek/bieg_D_prawanoga.png")
+bieg_S_lewanoga = image.load("assets/brajanek/bieg_S_lewanoga.png")
+bieg_S_prawanoga = image.load("assets/brajanek/bieg_S_prawanoga.png")
+bieg_W_lewanoga = image.load("assets/brajanek/bieg_W_lewanoga.png")
+bieg_W_prawanoga = image.load("assets/brajanek/bieg_W_prawanoga.png")
+stanie_A = image.load("assets/brajanek/stanie_A.png")
+stanie_D = image.load("assets/brajanek/stanie_D.png")
+stanie_S = image.load("assets/brajanek/stanie_S.png")
+stanie_W = image.load("assets/brajanek/stanie_W.png")
+porazka = image.load("assets/brajanek/porazka.png")
+bieg_lewo_gif = image.load("assets/brajanek/bieg_lewo.gif")
+
+wyglady = {"bieg_A_lewanoga": bieg_A_lewanoga,
+           "bieg_A_prawanoga": bieg_A_prawanoga,
+           "bieg_D_lewanoga": bieg_D_lewanoga,
+           "bieg_D_prawanoga": bieg_D_prawanoga,
+           "bieg_S_lewanoga": bieg_S_lewanoga,
+           "bieg_S_prawanoga": bieg_S_prawanoga,
+           "bieg_W_lewanoga": bieg_W_lewanoga,
+           "bieg_W_prawanoga": bieg_W_prawanoga,
+           "stanie_LEFT": stanie_A,
+           "stanie_RIGHT": stanie_D,
+           "stanie_DOWN": stanie_S,
+           "stanie_UP": stanie_W,
+           "porazka": porazka,
+           "bieg_lewo_gif": bieg_lewo_gif}
---
+++ main.py
@@ -0,0 +1,88 @@
+import sys
+import pygame
+from pygame import constants
+from pygame.constants import K_UP, K_RIGHT, K_LEFT, K_a, K_d, K_w, K_s, K_DOWN, K_ESCAPE
+from brajanek import Brajanek
+
+pygame.init()
+
+size = WINDOWWIDTH, WINDOWHEIGHT = 800, 600
+speed = [5, 5]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Brajanek Defence")
+pygame.display.set_icon(pygame.image.load("assets/icon.jpg"))
+
+background_image = pygame.image.load("assets/background.png")
+background_image = pygame.transform.scale(background_image, (WINDOWWIDTH, WINDOWHEIGHT))
+screen.blit(background_image, (0, 0))
+
+brajanek = Brajanek(WINDOWWIDTH, WINDOWHEIGHT)
+clock = pygame.time.Clock()
+running = True
+
+is_running = False  # Flag to track movement state
+current_direction = None  # Track the current movement direction
+
+while running:
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == constants.KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+            elif event.key == K_RIGHT or event.key == K_d:
+                brajanek.change_image("bieg_D_lewanoga")
+                is_running = True
+                current_direction = "RIGHT"
+            elif event.key == K_LEFT or event.key == K_a:
+                brajanek.change_image("bieg_A_lewanoga")
+                is_running = True
+                current_direction = "LEFT"
+            elif event.key == K_UP or event.key == K_w:
+                brajanek.change_image("bieg_W_lewanoga")
+                is_running = True
+                current_direction = "UP"
+            elif event.key == K_DOWN or event.key == K_s:
+                brajanek.change_image("bieg_S_lewanoga")
+                is_running = True
+                current_direction = "DOWN"
+        elif event.type == constants.KEYUP:
+            if (
+                    (event.key == K_RIGHT or event.key == K_d)
+                    and current_direction == "RIGHT"
+            ) or (
+                    (event.key == K_LEFT or event.key == K_a)
+                    and current_direction == "LEFT"
+            ) or (
+                    (event.key == K_UP or event.key == K_w)
+                    and current_direction == "UP"
+            ) or (
+                    (event.key == K_DOWN or event.key == K_s)
+                    and current_direction == "DOWN"
+            ):
+                brajanek.change_image("stanie_" + current_direction)
+                is_running = False
+                current_direction = None
+
+    if is_running:
+        if current_direction == "RIGHT":
+            brajanek.brajanekX += brajanek.speed[0]
+        elif current_direction == "LEFT":
+            brajanek.brajanekX -= brajanek.speed[0]
+        elif current_direction == "UP":
+            brajanek.brajanekY -= brajanek.speed[1]
+        elif current_direction == "DOWN":
+            brajanek.brajanekY += brajanek.speed[1]
+
+    brajanek.rect.center = (brajanek.brajanekX, brajanek.brajanekY)
+
+    screen.fill(black)
+    screen.blit(background_image, (0, 0))
+    screen.blit(brajanek.image, brajanek.rect)
+
+    pygame.display.flip()
+
+    clock.tick(60)  # Limit the frame rate to 60 FPS
